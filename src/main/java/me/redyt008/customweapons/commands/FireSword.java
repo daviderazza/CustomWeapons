4181f0bc10b5b68169f4ee76e5e9e06ebd673fbe
daviderazza <daviderazza86@gmail.com>
2023-12-22 16:47:50 +0100

    Changes

--- src/main/java/me/redyt008/customweapons/commands/FireSword.java
+++ src/main/java/me/redyt008/customweapons/commands/FireSword.java
@@ -2,15 +2,18 @@ package me.redyt008.customweapons.commands;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class FireSword implements CommandExecutor {
 
